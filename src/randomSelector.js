d0a13c624e3716963df53ebdb9d044f5feef9f8b
bernardo mendonça <bernardomendonca@outlook.com>
2021-09-30 16:47:00 +1000

    initial commit

---
+++ src/randomSelector.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+
+const address = '0x0783C5328fbcC035C926e20f64deE53A1b2adBF1';
+
+const ABI = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+//exporting a local instance of the contract
+export default new web3.eth.Contract(ABI, address);
+
